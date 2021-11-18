5413fa475afa95d7aa94262b5c26841dda64d5d3
Yutao Zhou <91305696+Yutao-Zhou@users.noreply.github.com>
2021-11-17 17:36:16 -0800

    Delete find-all-numbers-disappeared-in-an-array directory

--- find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        l = len(nums)
-        nums = set(nums)
-        ck = set(range(1,l + 1))
-        return ck.difference(nums)
