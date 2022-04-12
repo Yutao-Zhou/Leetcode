4ea37a5f0cda0dd37b911f375fc53f7dfc4cfcc7
Yutao Zhou <91305696+Yutao-Zhou@users.noreply.github.com>
2022-04-12 11:05:22 +0800

    recursive

--- 326. Power of Three.py
+++ 326. Power of Three.py
@@ -1,10 +1,22 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
         #### loop ####
-        if n < 1:
-            return False
-        while n > 2:
+        # if n < 1:
+        #     return False
+        # while n > 2:
+        #     if n % 3:
+        #         return False
+        #     n = n // 3
+        # return n == 1
+        
+        #### recursive ####
+        def recursive(n):
+            if n == 1:
+                return True
             if n % 3:
                 return False
-            n = n // 3
-        return n == 1
+            else:
+                return recursive(n // 3)
+        if n < 1:
+            return False
+        return recursive(n)
--- 342. Power of Four.py
+++ 342. Power of Four.py
@@ -1,10 +1,21 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
         #### loop ####
+        # if n < 1:
+        #     return False
+        # while n > 3:
+        #     if n % 4:
+        #         return False
+        #     n = n // 4
+        # return n == 1
+        
+        #### recursive ####
+        def recursive(num):
+            if num == 4 or num == 1:
+                return True
+            if num % 4 != 0:
+                return False
+            return recursive(num // 4)
         if n < 1:
             return False
-        while n > 3:
-            if n % 4:
-                return False
-            n = n // 4
-        return n == 1
+        return recursive(n)
